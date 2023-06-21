14141448c49f6548f1b22db31e8959137ebf2fff
TechNathan <111061907+TechNathan@users.noreply.github.com>
2023-06-21 23:11:12 +0800

    newDaffodilNumber.py

--- newDaffodilNumber.py
+++ newDaffodilNumber.py
@@ -3,7 +3,7 @@ print("This is going to verify whether your number is daffodilNumber or not")
 number = int(input("Type in your number: "))
 thirdNumber = number % 10
 secondNumber = number // 10 % 10
-firstNumber = number // 100 % 10
+firstNumber = number // 100
 if math.pow(thirdNumber, 3)+math.pow(secondNumber, 3)+math.pow(firstNumber, 3)==number:
     print("This number is a daffodilNumber.")
 else:
